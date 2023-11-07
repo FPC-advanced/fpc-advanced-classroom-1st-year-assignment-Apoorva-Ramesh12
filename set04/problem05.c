a118d5cb6ca5f65ca409589a94749c2596364193
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-11-07 11:31:26 +0530

    committed 11/7/2023, 11:31:26 AM

--- set04/problem05.c
+++ set04/problem05.c
@@ -4,3 +4,45 @@ int input_size();
 void input_array(int n, int a[n]);
 int find_largest_index(int n, int a[n]);
 void output(int index);
+int main()
+{
+    int n;
+    n=input_size();
+    int a[n];
+    input_array(n,a);
+    int index;
+    index=find_largest_index(n,a);
+    output(index);
+    return 0;
+}
+int input_size()
+{
+    int n;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    return n;
+}
+void input_array(int n, int a[n])
+{
+    printf("Enter the numbers:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+}
+int find_largest_index(int n, int a[n])
+{
+    int largest=a[0],index;
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]>largest)
+        {
+            index=i;
+        }
+    }
+    return index;
+}
+void output(int index)
+{
+    printf("The index of the largest number in the array is %d",index);
+}
