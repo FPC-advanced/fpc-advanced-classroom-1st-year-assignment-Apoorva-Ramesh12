1df23be576a5c142d72ca0b28eb5d9f5e7e5817b
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-11-10 00:53:06 +0530

    committed 11/10/2023, 12:53:04 AM

---
+++ set05/problem08.c
@@ -0,0 +1,15 @@
+//Write program to find the weight of a truck load of n camels.
+// Take inputs for:
+// - truck weight
+// - height, radius and length of n different camels
+
+// total_truck_weight = truck_weight + weight_of_camels
+#include<stdio.h>
+ypedef struct _camel {
+    float radius, height, length, weight;
+} Camel;
+
+void input(int n, Camel c[n], float *truck_weight);
+void find_camel_weight(int n, Camel c[n]);
+float compute_total_weight(int n, Camel c[n], float truck_weight);
+void output(float total_weight);
