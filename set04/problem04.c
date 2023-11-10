27cf8cb96a3a78396047e6e5c17cd1a86b7a113d
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-11-10 15:04:44 +0530

    committed 11/10/2023, 3:04:43 PM

--- set04/problem04.c
+++ set04/problem04.c
@@ -5,3 +5,52 @@ void input_coefficients(int n, float a[n]);
 float input_x();
 float evaluate_polynomial(int n, float a[n], float x);
 void output(int n, float a[n], float x, float result);
+int main()
+{
+    int d;
+    d=input_degree();
+    int n;
+    float a[n];
+    input_coefficients(n,a);
+    float x;
+    x=input_x();
+    float result;
+    result=evaluate_polynomial(n,a,x);
+    output(n,a,x,result);
+    return 0;
+}
+int input_degree()
+{
+    int x;
+    printf("Enter the degree of the polynomial:");
+    scanf("%d",&x);
+    return x;
+}
+void input_coefficients(int n, float a[n])
+{
+    printf("Enter the co-efficients of the polynomial:");
+    for(int i=0;i<=n;i++)
+    {
+        scanf("%f",&a[i]);
+    }
+}
+float input_x()
+{
+    float x;
+    printf("Enter the value of x:");
+    scanf("%f",&x);
+    return x;
+}
+float evaluate_polynomial(int n,float a[n],float x)
+{
+    float result; //result=a[n]
+    for(int i=n-1;i>=0;i++)
+    {
+        result=result*x+a[i];
+    }
+    return result;
+}
+void output(int n, float a[n], float x, float result)
+{
+    printf("The polynomial evaluated at x=%.2f is %.2f\n",x,result);
+}
