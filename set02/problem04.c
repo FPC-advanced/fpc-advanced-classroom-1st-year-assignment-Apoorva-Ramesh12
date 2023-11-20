3612845ce91002e2cb89b730c787862761d8a54e
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-11-20 19:15:55 +0530

    committed 11/20/2023, 7:15:54 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -35,6 +35,7 @@ int sum_composite_numbers(int n,int a[n])
     int sum=0;
     for(int i=0;i<=n;i++)
     {
+        int composite=0;
         for(int j=2;j<a[i];j++)
         {
             if(a[i]%j==0)
