b69795dd11fda879d2ccfb62c780a528134746ef
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-11-21 12:27:17 +0530

    committed 11/21/2023, 12:27:16 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -23,51 +23,7 @@ void input_two_strings(char *string1, char *string2)
 int stringcompare(char *string1, char *string2)
 {
     int largest;
-    for(int i=0,j=0;string1[i]!='\0' || string2[j]!='\0';i++,j++)
-    {   
-        if(string1[i]==string2[j])
-        {
-            int string1[i],string2[j];
-            
-            if(string1[i]>string2[j])
-            {
-                largest=1;
-            }
-            else{
-                largest=2;
-            }
-        }
-        else if(string1[i]<string2[j])
-        {
-            largest=2;
-        }
-        else if(string1[i]>string2[j])
-        {
-            largest=1;
-        }
-        else
-        {
-            largest=0;
-        }
-        if(string1[i]!='\0' && string2[i]=='\0')
-        {
-            largest=2;
-        }
-        else
-        {
-            largest=1;
-        }
-        if(string1[i]=='/0' && string2[j]!='\0')
-        {
-            largest=1;
-        }
-        else
-        {
-            largest=2;
-        }
-
-    }
-    return largest;
+    
 }
 void output(char *string1, char *string2, int result)
 {
