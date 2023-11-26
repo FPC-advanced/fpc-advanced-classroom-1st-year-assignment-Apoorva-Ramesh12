96cea46cc7fc5390748514f1194624ba47aeffcf
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-11-26 17:21:51 +0530

    committed 11/26/2023, 5:21:50 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -36,18 +36,9 @@ int stringcompare(char *string1, char *string2)
         else
         {
             largest=1;
-        }
-        if(largest==0)
-        {
-            if(string1[i]=='\0' && string2[i]!='\0')
-            {
-                largest=1;
-            }
-            else{
-                largest=2;
-            }
-        }
+        }    
     }
+    
     return largest;
 }
 void output(char *string1, char *string2, int result)
