befd43eea20e019b9da8d8bacac60233b1744646
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-11-30 13:52:25 +0530

    committed 11/30/2023, 1:52:23 PM

--- set03/problem05_chatgpt.c
+++ set03/problem05_chatgpt.c
@@ -1,49 +1,44 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-int input_array_size();
-void init_array(int n, bool a[n]);
-void erotosthenes_sieve(int n, bool a[n]);
-void output(int n, bool a[n]);
-
-int main() {
-    int n;
-    n = input_array_size();
-    bool a[n];
-    init_array(n, a);
-    erotosthenes_sieve(n, a);
-    output(n, a);
-    return 0;
-}
+#include<stdio.h>
+#include<stdbool.h>
 
 int input_array_size() {
     int n;
-    printf("Enter the size of the array:");
+    printf("Enter the value of n: ");
     scanf("%d", &n);
     return n;
 }
 
-void init_array(int n, bool a[n]) {
-    for (int i = 2; i < n; i++) {
-        a[i] = true;
+void init_array(int n, bool a[n+1]) {
+    for (int i = 0; i <= n; i++) {
+        a[i] = true; // Initialize array elements as true
     }
 }
 
-void erotosthenes_sieve(int n, bool a[n]) {
-    for (int i = 2; i * i <= n; i++) {
-        if (a[i] == true) {
-            for (int j = i * i; j < n; j += i) {
-                a[j] = false;
+void eratosthenes_sieve(int n, bool a[n+1]) {
+    for (int p = 2; p * p <= n; p++) {
+        if (a[p] == true) {
+            for (int i = p * p; i <= n; i += p) {
+                a[i] = false; // Mark multiples of p as false (not prime)
             }
         }
     }
 }
 
-void output(int n, bool a[n]) {
+void output(int n, bool a[n+1]) {
     printf("Prime numbers between 2 and %d are:\n", n);
-    for (int i = 2; i < n; i++) {
+    for (int i = 2; i <= n; i++) {
         if (a[i] == true) {
-            printf("%d\n", i);
+            printf("%d ", i);
         }
     }
+    printf("\n");
+}
+
+int main() {
+    int n = input_array_size();
+    bool arr[n+1];
+    init_array(n, arr);
+    eratosthenes_sieve(n, arr);
+    output(n, arr);
+    return 0;
 }
