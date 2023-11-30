cb3019a91da69cb1cbc3efe46d0caaeebbc86b52
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-11-30 10:53:09 +0530

    committed 11/30/2023, 10:53:07 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -25,11 +25,7 @@ int stringcompare(char *string1, char *string2)
     int largest=0, i = 0;
     while(string1[i]!='\0' && string2[i]!='\0')
     {    
-        if(string1[i]==string2[i])
-        {
-            i++;
-        }
-        if(string1[i]>string2[i])
+        else if(string1[i]>string2[i])
         {
             largest=1;
             break;
