bb6912007de96665d40007d62be1128a4941e56c
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-12-03 20:45:33 +0530

    committed 12/3/2023, 8:45:32 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -26,16 +26,6 @@ int stringcompare(char *string1, char *string2)
     for(i=0;(string1[i]!='\0' || string2[i]!='\0') && string1[i]==string2[i];i++);
     printf("%d %d",string1[i],string2[i]);
     return string1[i]-string2[i];
-    // if(string1[i]>string2[i])
-    // {
-        
-    //     largest=1;   
-    // }
-    // else 
-    // {
-    //     largest=2;
-    // }   
-    // return largest;   
 }
 void output(char *string1, char *string2, int result)
 {
