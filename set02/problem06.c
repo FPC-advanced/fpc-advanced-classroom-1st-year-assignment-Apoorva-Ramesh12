a0cfbda4d2b8a8644d829dcbe2885e065a9c4005
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-12-04 14:48:41 +0530

    committed 12/4/2023, 2:48:40 PM

--- set02/problem06.c
+++ set02/problem06.c
@@ -22,11 +22,11 @@ void str_reverse(char *str, char *rev_str)
 {
     int len = strlen(str);
     int i, j = 0;
-    for (i = len - 1; i >= 0; i--)
-    {
-        rev_str[j] = str[i];
-        j++;
-    }
+    // for (i = len - 1; i >= 0; i--)
+    // {
+    //     rev_str[j] = str[i];
+    //     j++;
+    // }
     //rev_str[j] = '\0';
 }
 void output(char *a, char *rev_str)
