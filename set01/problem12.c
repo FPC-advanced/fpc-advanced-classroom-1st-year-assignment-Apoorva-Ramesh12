1d7ead90d953acb9b0a527ff712966f73acdc623
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-12-04 12:06:37 +0530

    committed 12/4/2023, 12:06:36 PM

--- set01/problem12.c
+++ set01/problem12.c
@@ -64,12 +64,12 @@ Complex add_n_complex(int n, Complex c[n])
 }
 void output(int n, Complex c[n], Complex result)
 {
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n-1;i++)
     {
         printf("%f+%fi",c[i].real,c[i].imaginary);
         printf(" + ");       
     }
-    printf("%f+%f =  %f+%fi\n",result.real,result.imaginary);
+    printf("%f+%fi =  %f+%fi\n",c[n].real,c[n].imaginary,result.real,result.imaginary);
 
        
     
