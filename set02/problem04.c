2344137332d29eef142908e50756e4cce12bf462
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-12-04 16:15:24 +0530

    committed 12/4/2023, 4:15:23 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int input_array_size();
 void input_array(int n, int a[n]);
-int is_composite(int n);
+int is_composite(int n,int a[n]);
 int sum_composite_numbers(int n, int a[n]);
 void output(int sum);
 int main()
@@ -37,19 +37,6 @@ int sum_composite_numbers(int n,int a[n])
     int sum=0;
     for(int i=0;i<n;i++)
     {
-        int composite=0;
-        if(a[i]<=1)
-        {
-            continue;
-        }
-        for(int j=2;j<=a[i];j++)
-        {
-            if(a[i]%j==0)
-            {
-                composite=1;
-                break;
-            }
-        }
         if(composite==1)
         {
             sum=sum+a[i];
