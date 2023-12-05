271a60ad4dfb3addb72d75f8a386010612da7369
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2023-12-05 07:56:30 +0530

    committed 12/5/2023, 7:56:29 AM

--- set02/problem08.c
+++ set02/problem08.c
@@ -74,8 +74,8 @@ void output(int n,Triangle t[n],Triangle smallest)
 {
 	for(int i=0;i<n;i++)
 	{
-		printf("Triangle No. %d"\n,i+1);
-		printf("\nBase=%.2f  Altitude=%.2f\n",t[i].base,t[i].altitude);
+		printf("Triangle No. %d\n",i+1);
+		printf("Base=%.2f  Altitude=%.2f\n",t[i].base,t[i].altitude);
 	}
 	printf("Smallest triangle:\n Base: %.2f Altitude: %.2f Area: %.2f",smallest.base,smallest.altitude,smallest.area);
 }
