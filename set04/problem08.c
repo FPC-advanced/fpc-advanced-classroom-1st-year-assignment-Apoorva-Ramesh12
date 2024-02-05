1190b68c5c52636214864d73d5ed3ee1d439ac7c
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2024-02-05 16:29:20 +0530

    committed 2/5/2024, 4:29:19 PM

--- set04/problem08.c
+++ set04/problem08.c
@@ -79,7 +79,10 @@ Fraction add_n_fractions(int n, Fraction f[n])
 }
 void output(int n, Fraction f[n], Fraction sum)
 {
-    if(sum.den)
+    if(sum.den==1)
+    {
+        
+    }
     int i=0;
     for(int i=0;i<n-1;i++)
     {
