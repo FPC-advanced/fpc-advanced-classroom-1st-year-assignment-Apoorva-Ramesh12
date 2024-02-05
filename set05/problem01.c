35c5bd4a7e0aed88eb7023a01a001017bc56155e
Apoorva-Ramesh12 <apoorvar.bsc23@rvu.edu.in>
2024-02-05 16:48:21 +0530

    committed 2/5/2024, 4:48:19 PM

--- set05/problem01.c
+++ set05/problem01.c
@@ -11,7 +11,9 @@ void dist(Point a, Point b, float *res);
 void output(Point a, Point b, float res);
 int main()
 {
-  Point p1,p2;
-  p1=input();
-  p2=input()
+  Point a,b;
+  a=input();
+  b=input();
+  
+
 }
